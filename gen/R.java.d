0211634e1eeb88ef240dce468d343cf702c49a9a
Meï-Garino Jérémy <jeremy@c-g-m.org>
2013-09-24 08:36:28 +0200

    Delete R.java.d

--- gen/R.java.d
+++
@@ -1,9 +0,0 @@
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\gen\com\thebigbang\dust\R.java \
- : C:\Users\thebigbang\Documents\NetBeansProjects\Dust\res\drawable-hdpi\ic_launcher.png \
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\res\drawable-mdpi\ic_launcher.png \
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\res\drawable-xhdpi\ic_launcher.png \
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\res\values\strings.xml \
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\res\values\styles.xml \
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\res\values-v11\styles.xml \
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\res\values-v14\styles.xml \
-C:\Users\thebigbang\Documents\NetBeansProjects\Dust\bin\AndroidManifest.xml \
